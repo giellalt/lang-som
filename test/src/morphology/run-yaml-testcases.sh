7763d01a27ca68f6a2ecdcd89d96fe2063173b04
Ryan Johnson <ryan.txanson@gmail.com>
2013-04-30 17:53:10 +0000

    Beginning to move over CG, twolc, lexica. Some of these work, some do not, but not concerned about that yet.

---
+++ test/src/morphology/run-yaml-testcases.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# This is a shell script that will loop over the *.yaml files of the specified
+# transducer type found in this dir
+# and run them through the Apertium Quality morph-test.py tool.
+#
+# It takes one argument: the transducer type.
+#
+# The script is typically called by a simple shell script listed in the TESTS
+# variable in the Makefile.am file, and will then be automatically called when
+# issuing 'make check'
+#
+# This script only contains the loop, and will for each found yaml file call
+# the actual test runner.
+
+# Wrong usage - short instruction:
+if ! test $# -ge 1 ; then
+    echo
+    echo "Usage: $0 TRANSDUCERTYPE"
+    echo
+    echo "were TRANSDUCERTYPE is the type of transducer targeted"
+    echo "for testing: gt-norm, gt-desc, etc. It will loop"
+    echo "over all yaml tests for the specified TRANSDUCERTYPE."
+    echo
+    exit 77
+fi
+
+###### Variables: #######
+transducer=$1
+Fail=0
+
+testfiles=$(find ${srcdir} -name "*_$transducer.yaml")
+if test "$testfiles" == ""; then
+    echo
+    echo "*** Warning: No YAML data files found. Skipping test."
+    echo "*** Filenames covered by this test needs to end in:"
+    echo "***"
+    echo "*** _$transducer.yaml"
+    echo
+    exit 77
+fi
+
+i=0
+# Loop over the available yaml files, and run the tests:
+for file in ${srcdir}/*_$transducer.yaml; do
+    (( i += 1 ))
+    leadtext=$(echo "YAML test $i: ")
+	source ./run-morph-tester.sh $transducer $file $leadtext
+done
+
+source error-handling-stubs.sh
